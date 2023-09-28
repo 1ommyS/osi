5e67a65baddb48a74d8bd5b3cff1f2df493d0220
Ivan <54151565+1ommyS@users.noreply.github.com>
2023-09-28 20:38:21 +0300

    ewew

--- child.c
+++ child.c
@@ -1,52 +1,72 @@
-#include <fcntl.h>
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 
 const int MAX_LENGTH_OF_LINE = 30;
 const int MAX_AMOUNT_OF_LINES = 100;
 const int FILENAME_SIZE = 10;
 
-void reverse_lines(char* lines) {
-  int len = strlen(lines);
-  for (int i = 0; i < len / 2; i++) {
-    char temp = lines[i];
-    lines[i] = lines[len - i - 1];
-    lines[len - i - 1] = temp;
-  }
+size_t strlen(const char *str) {
+    size_t length = 0;
+    while (*str != '\0') {
+        length++;
+        str++;
+    }
+    return length;
+}
+void reverse_lines(char lines[MAX_AMOUNT_OF_LINES][MAX_LENGTH_OF_LINE], int len) {
+    for (int i = 0; i < len; i++) {
+        int start = 0;
+        int end = strlen(lines[i]) - 1;
+        while (start < end) {
+            char temp = lines[i][start];
+            lines[i][start] = lines[i][end];
+            lines[i][end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
+void write_lines_to_file(char lines[MAX_AMOUNT_OF_LINES][MAX_LENGTH_OF_LINE],
+                         char filename[FILENAME_SIZE], int len) {
+    FILE *file = fopen(filename, "w");
+
+    if (file == NULL) {
+        printf("Failed to open the file. Exiting.\n");
+        exit(-1);
+    } else {
+        printf("File opened successfully.\n");
+    }
+
+    for (int index_of_line = 0;
+         index_of_line < len && lines[index_of_line][0] != EOF; ++index_of_line) {
+        fprintf(file, "%s\n", lines[index_of_line]);
+    }
+
+    fclose(file);
 }
 
-void write_lines_to_file(char lines[MAX_AMOUNT_OF_LINES][MAX_LENGTH_OF_LINE], char filename[FILENAME_SIZE]) {
-  FILE* file = open(filename, "w");
+int main(int argc, char *argv[]) {
+    int amount_of_lines = atoi(argv[0]);
 
-  if (file == NULL) {
-    printf("file wasn't open");
-    exit(-1);
-  }
+    char filename[FILENAME_SIZE];
+    read(STDIN_FILENO, &filename, sizeof(filename));
 
-  for (int index_of_line = 0; index_of_line < MAX_AMOUNT_OF_LINES && lines[index_of_line][0] != EOF; ++index_of_line) {
-    fprintf(file, "%s\n", lines[index_of_line]);
-    printf("%s\n", lines[index_of_line]);
-  }
+    char lines[amount_of_lines][MAX_LENGTH_OF_LINE];
+    for (int amount_of_line_from_input = 0;
+         amount_of_line_from_input < amount_of_lines;
+         amount_of_line_from_input++) {
+        read(STDIN_FILENO, lines[amount_of_line_from_input],
+             sizeof(lines[amount_of_line_from_input]));
+    }
 
-  fclose(file);
-}
+    reverse_lines(lines, amount_of_lines);
 
-int main() {
-  char filename[FILENAME_SIZE];
-  read(STDIN_FILENO, &filename, sizeof(filename));
+    printf("Реверсированные строки:\n");
+    for (int line_index = 0; line_index < amount_of_lines; line_index++) {
+        printf("%s\n", lines[line_index]);
+    }
 
-  char lines[MAX_AMOUNT_OF_LINES][MAX_LENGTH_OF_LINE];
-  for (int amount_of_line_from_input = 0;
-       amount_of_line_from_input < MAX_AMOUNT_OF_LINES;
-       amount_of_line_from_input++) {
-    read(STDIN_FILENO, lines[amount_of_line_from_input],
-         sizeof(lines[amount_of_line_from_input]));
-  }
-
-  reverse_lines(lines);
-
-  write_lines_to_file(lines, filename);
+    write_lines_to_file(lines, filename, amount_of_lines);
+    close(STDIN_FILENO);
 }
--- main.c
+++ main.c
@@ -9,115 +9,129 @@ const int MAX_AMOUNT_OF_LINES = 100;
 const int FILENAME_SIZE = 10;
 
 void handle_error(int result_of_statement) {
-  if (result_of_statement == -1) {
-    perror("error with creating: ");
-    exit(-1);
-  }
+    if (result_of_statement == -1) {
+        perror("error with creating: ");
+        exit(-1);
+    }
 }
 
 pid_t try_to_create_proccess() {
-  pid_t proccess_id = fork();
-  handle_error(proccess_id);
-  return proccess_id;
+    pid_t proccess_id = fork();
+    handle_error(proccess_id);
+    return proccess_id;
 }
 
 int main() {
-  char file_first_name[FILENAME_SIZE];
-  char file_second_name[FILENAME_SIZE];
+    char file_first_name[FILENAME_SIZE];
+    char file_second_name[FILENAME_SIZE];
 
-  char lines[MAX_AMOUNT_OF_LINES][MAX_LENGTH_OF_LINE];
+    char lines[MAX_AMOUNT_OF_LINES][MAX_LENGTH_OF_LINE];
 
-  printf("Имя первого файла: ");
-  scanf("%s", &file_first_name);
-  printf("Имя второго файла: ");
-  scanf("%s", &file_second_name);
+    printf("Имя первого файла: ");
+    scanf("%s", &file_first_name);
+    printf("Имя второго файла: ");
+    scanf("%s", &file_second_name);
 
-  int amount_lines;
-  printf("Теперь введите количество строк. Оно должно быть меньше %d :",
-         MAX_AMOUNT_OF_LINES);
-  scanf("%d", &amount_lines);
+    int amount_lines;
+    printf("Теперь введите количество строк. Оно должно быть меньше %d : ",
+           MAX_AMOUNT_OF_LINES);
+    scanf("%d", &amount_lines);
 
-  printf("Теперь вводите сами строки.\n");
+    printf("Теперь вводите сами строки.\n");
 
-  char lines_for_first_proccess[MAX_AMOUNT_OF_LINES][MAX_LENGTH_OF_LINE];
-  char lines_for_second_proccess[MAX_AMOUNT_OF_LINES][MAX_LENGTH_OF_LINE];
+    char lines_for_first_proccess[MAX_AMOUNT_OF_LINES][MAX_LENGTH_OF_LINE];
+    char lines_for_second_proccess[MAX_AMOUNT_OF_LINES][MAX_LENGTH_OF_LINE];
 
-  int amount_of_lines_for_first_proccess = 0;
-  int amount_of_lines_for_second_proccess = 0;
+    int amount_of_lines_for_first_proccess = 0;
+    int amount_of_lines_for_second_proccess = 0;
 
-  for (int index_of_current_line = 0; index_of_current_line < amount_lines;
-       index_of_current_line++) {
-    if (rand() % 100 <= 80) {
-      scanf("%s",
-            lines_for_first_proccess[amount_of_lines_for_first_proccess++]);
+    for (int index_of_current_line = 0; index_of_current_line < amount_lines;
+         index_of_current_line++) {
+        int rand = arc4random() % 100;
+        printf("Rand = %d\n", rand);
+        if (rand <= 80) {
+            scanf("%s",
+                  lines_for_first_proccess[amount_of_lines_for_first_proccess++]);
+        } else {
+            scanf("%s",
+                  lines_for_second_proccess[amount_of_lines_for_second_proccess++]);
+        }
+    }
+
+    lines_for_first_proccess[amount_of_lines_for_first_proccess][0] = EOF;
+    lines_for_second_proccess[amount_of_lines_for_second_proccess][0] = EOF;
+
+    int pipe_for_first_child[2];
+
+    handle_error(pipe(pipe_for_first_child));
+
+    pid_t first_proccess_id = try_to_create_proccess();
+
+    if (first_proccess_id == 0) {
+        printf("First child has been started.\n");
+
+        close(pipe_for_first_child[1]);
+        dup2(pipe_for_first_child[0], STDIN_FILENO);
+
+        char amount_lines_str[MAX_LENGTH_OF_LINE];
+        sprintf(amount_lines_str, "%d", amount_of_lines_for_first_proccess);
+
+        execl("child", amount_lines_str, NULL);
+        perror("мы не зашли в дочерний процесс #1");
     } else {
-      scanf("%s",
-            lines_for_second_proccess[amount_of_lines_for_second_proccess++]);
+        printf("parent process\n");
+
+        close(pipe_for_first_child[0]);
+
+        write(pipe_for_first_child[1], &file_first_name, sizeof(file_first_name));
+
+        for (int current_line = 0;
+             current_line < amount_of_lines_for_first_proccess; ++current_line) {
+            write(pipe_for_first_child[1], &lines_for_first_proccess[current_line],
+                  sizeof(lines_for_first_proccess[current_line]));
+        }
+        close(pipe_for_first_child[1]);
+        printf("Данные отправились в первый процесс\n");
+
+        wait(NULL);
+
+        printf("Первый процесс завершилcя\n");
     }
-  }
 
-  lines_for_first_proccess[amount_of_lines_for_first_proccess][0] = EOF;
-  lines_for_second_proccess[amount_of_lines_for_second_proccess][0] = EOF;
+    int pipe_for_second_child[2];
 
-  int pipe_for_first_child[2];
+    handle_error(pipe(pipe_for_second_child));
 
-  handle_error(pipe(pipe_for_first_child));
+    pid_t second_proccess_id = try_to_create_proccess();
 
-  pid_t first_proccess_id = try_to_create_proccess();
+    if (second_proccess_id == 0) {
+        printf("First child has been started.\n");
 
-  if (first_proccess_id == 0) {
-    printf("First child has been started.\n");
+        close(pipe_for_second_child[1]);
+        dup2(pipe_for_second_child[0], STDIN_FILENO);
 
-    close(pipe_for_first_child[1]);
-    dup2(pipe_for_first_child[0], STDIN_FILENO);
-    execl("./child", "./child", NULL);
-  } else {
-    printf("parent process\n");
+        char amount_lines_str[MAX_LENGTH_OF_LINE];
+        sprintf(amount_lines_str, "%d", amount_of_lines_for_second_proccess);
 
-    close(pipe_for_first_child[0]);
-    write(pipe_for_first_child[1], &file_first_name, sizeof(file_first_name));
+        execl("child", amount_lines_str, NULL);
+        perror("мы не зашли в дочерний процесс #2");
+    } else {
+        printf("parent process\n");
 
-    for (int current_line = 0; current_line < amount_of_lines_for_first_proccess;
-         ++current_line) {
-      write(pipe_for_first_child[1], &lines_for_first_proccess[current_line],
-            sizeof(lines_for_first_proccess[current_line]));
+        close(pipe_for_second_child[0]);
+
+        write(pipe_for_second_child[1], &file_second_name, sizeof(file_second_name));
+
+        for (int current_line = 0;
+             current_line < amount_of_lines_for_second_proccess; ++current_line) {
+            write(pipe_for_second_child[1], &lines_for_second_proccess[current_line],
+                  sizeof(lines_for_second_proccess[current_line]));
+        }
+        close(pipe_for_second_child[1]);
+        printf("Данные отправились во второй процесс\n");
+
+        wait(NULL);
+
+        printf("Второй процесс завершилcя\n");
     }
-    close(pipe_for_first_child[1]);
-    printf("Данные отправились в первый процесс");
-
-    wait(NULL);
-
-    printf("Первый процесс завершилась");
-  }
-
-  int pipe_for_second_child[2];
-
-  handle_error(pipe(pipe_for_second_child));
-
-  pid_t second_proccess_id = try_to_create_proccess();
-
-  if (second_proccess_id == 0) {
-    printf("Second child has been started.\n");
-
-    close(pipe_for_second_child[1]);
-    dup2(pipe_for_second_child[0], STDIN_FILENO);
-    execl("./child", "./child", NULL);
-  } else {
-    printf("parent process\n");
-
-    close(pipe_for_second_child[0]);
-    write(pipe_for_second_child[1], &file_second_name, sizeof(file_second_name));
-
-    for (int current_line = 0; current_line < amount_of_lines_for_second_proccess;
-         ++current_line) {
-      write(pipe_for_second_child[1], &lines_for_second_proccess[current_line],
-            sizeof(lines_for_second_proccess[current_line]));
-    }
-    close(pipe_for_second_child[1]);
-    printf("Данные отправились во второй процесс");
-
-    wait(NULL);
-
-    printf("Первый процесс завершилась");
-  }
 }
