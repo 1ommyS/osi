2790e09f00011e41c28f543c4913c6814134a0ff
Ivan <54151565+1ommyS@users.noreply.github.com>
2023-11-24 20:05:58 +0300

    add makefile

--- firstLib/functions.cpp
+++ firstLib/functions.cpp
@@ -9,31 +9,5 @@ float firstWayToFindDerivative(float a, float deltaX)
 
 float secondWayToFindDerivative(float a, float deltaX)
 {
-	return (cos(a + deltaX) - cos(a - deltaX)) / 2 * deltaX;
-}
-
-int simple(int a, int b)
-{
-	int res = 0;
-	for (int i = a; i <= b; ++i)
-	{
-		bool flag = 1;
-		for (int d = 2; d < i; ++d)
-		{
-			if (i == 2)
-			{
-				break;
-			}
-			if (i % d == 0)
-			{
-				flag = 0;
-				break;
-			}
-		}
-		if (flag)
-		{
-			res += 1;
-		}
-	}
-	return res;
+	return (cos(a + deltaX) - cos(a - deltaX)) / (2 * deltaX);
 }
--- firstLib/main.cpp
+++ firstLib/main.cpp
@@ -5,15 +5,15 @@ int main(int argc, char *argv[])
 {
 	if (argc != 4)
 	{
-		std::cout << "Usage: func(1/2) leftborder rightborder" << std::endl;
+		std::cout << "Usage: func(1/2) ∂x or 2*∂x" << std::endl;
 	}
 	else if (std::atoi(argv[1]) == 1)
 	{
-		std::cout << "First way to find derivative. Result: " << firstWayToFindDerivative(std::atoi(argv[2]), std::atoi(argv[3])) << "\n";
+		std::cout << "First way to find derivative. Result: " << firstWayToFindDerivative(std::atof(argv[2]), std::atof(argv[3])) << "\n";
 	}
-	else if (std::atoi(argv[1]) == 2)
+	else if (std::atol(argv[1]) == 2)
 	{
-		std::cout << "Second way to find derivative. Result: " << secondWayToFindDerivative(std::atoi(argv[2]), std::atoi(argv[3])) << "\n";
+		std::cout << "Second way to find derivative. Result: " << secondWayToFindDerivative(std::atof(argv[2]), std::atof(argv[3])) << "\n";
 	}
 	else
 	{
